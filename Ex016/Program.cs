8bbed69719e7e291d67958d8ace3c7c919d086b0
cauagoncalves-p <cauagoncalves2190@gmail.com>
2025-01-22 21:27:42 -0300

    tabuada usando for

---
+++ Ex016/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número que você quer ver a tabuada: ");
+            int tab = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o número que você quer que va a tabuada: ");
+            int total = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= total; i++)
+            {
+                int resultado = tab * i;
+                Console.WriteLine("{0} X {1} = {2} ", tab, i, resultado);
+            }
+        }
+    }
+}
